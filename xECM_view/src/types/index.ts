cbf2e9e6076f31bec9d1890627049a9e9379b8d8
aice <falcode.business@gmail.com>
2026-01-20 11:46:46 +0900

    fix build error

--- xECM_view/src/types/index.ts
+++ xECM_view/src/types/index.ts
@@ -21,3 +21,12 @@ export type Toast = {
   message: string
   type: 'success' | 'info'
 }
+
+// ファイルデータの型定義
+export interface FileData {
+  id: string
+  filename: string
+  Mimetype: string
+  createdon: string
+  selected?: boolean
+}
