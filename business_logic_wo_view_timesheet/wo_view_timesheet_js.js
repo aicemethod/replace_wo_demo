96f36c9392bd2f9e16852e49facef1087586d0f7
aice <falcode.business@gmail.com>
2026-02-04 16:03:27 +0900

    new logic

--- business_logic_wo_view_timesheet/wo_view_timesheet_js.js
+++ business_logic_wo_view_timesheet/wo_view_timesheet_js.js
@@ -1,22 +1,24 @@
 function viewTimesheet(primaryControl) {
 
-    // グローバルコンテキスト取得
     var globalContext = Xrm.Utility.getGlobalContext();
 
-    // 現在のアプリ ID を取得
-    var appId = globalContext.getCurrentAppId();
+    globalContext.getCurrentAppProperties().then(
+        function (appProperties) {
 
-    // Web リソース名（固定）
-    var webResourceName = "new_/Timesheet.html";
+            var appId = appProperties.appId;
+            var webResourceName = "new_/Timesheet.html";
 
-    // URL 組み立て
-    var url =
-        globalContext.getClientUrl()
-        + "/main.aspx"
-        + "?appid=" + appId
-        + "&pagetype=webresource"
-        + "&webresourceName=" + encodeURIComponent(webResourceName);
+            var url =
+                globalContext.getClientUrl()
+                + "/main.aspx"
+                + "?appid=" + appId
+                + "&pagetype=webresource"
+                + "&webresourceName=" + encodeURIComponent(webResourceName);
 
-    // 別タブで開く
-    window.open(url, "_blank");
+            window.open(url, "_blank");
+        },
+        function (error) {
+            console.error(error.message);
+        }
+    );
 }
