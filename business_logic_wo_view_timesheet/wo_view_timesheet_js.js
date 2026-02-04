c593e3ecbe96639810dda539a58bf46a85d0e694
aice <falcode.business@gmail.com>
2026-02-04 16:15:59 +0900

    wo id param out

--- business_logic_wo_view_timesheet/wo_view_timesheet_js.js
+++ business_logic_wo_view_timesheet/wo_view_timesheet_js.js
@@ -7,13 +7,34 @@ function viewTimesheet(primaryControl) {
 
             var appId = appProperties.appId;
             var webResourceName = "new_/Timesheet.html";
+            // 現在開いているWO（フォーム）のレコードIDを取得し、Timesheet側に渡す
+            // timesheet_view は URL パラメータ recordid を見て selectedWO を初期化する
+            var recordId = null;
+            try {
+                if (
+                    primaryControl &&
+                    primaryControl.data &&
+                    primaryControl.data.entity &&
+                    typeof primaryControl.data.entity.getId === "function"
+                ) {
+                    recordId = primaryControl.data.entity.getId();
+                }
+            } catch (e) {
+                // 取得できない場合は null のまま（全件表示などにフォールバック）
+                recordId = null;
+            }
+            if (recordId) {
+                // "{GUID}" -> "GUID"
+                recordId = recordId.replace(/[{}]/g, "");
+            }
 
             var url =
                 globalContext.getClientUrl()
                 + "/main.aspx"
                 + "?appid=" + appId
                 + "&pagetype=webresource"
-                + "&webresourceName=" + encodeURIComponent(webResourceName);
+                + "&webresourceName=" + encodeURIComponent(webResourceName)
+                + (recordId ? ("&recordid=" + encodeURIComponent(recordId)) : "");
 
             window.open(url, "_blank");
         },
