164f7cbe233cc91b94baa064a7af4e0928a29ea6
aice <falcode.business@gmail.com>
2026-03-04 18:34:49 +0900

    サイドバー追加

--- timesheet/src/App.tsx
+++ timesheet/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import type { JSX } from 'react'
 import { useDataverseQuery } from './hooks/useDataverseQuery'
-import { CalendarController, Header } from './components'
+import { CalendarController, Header, Sidebar } from './components'
 
 type Account = {
   accountid: string
@@ -19,12 +19,25 @@ function App(): JSX.Element {
       value: account.accountid,
       label: account.name,
     })) ?? []
+  const users =
+    data?.map((account) => ({
+      id: account.accountid,
+      number: account.accountnumber ?? '',
+      name: account.name,
+    })) ?? []
   const formattedDate = new Intl.DateTimeFormat('ja-JP').format(new Date())
 
   return (
     <div className="app">
       <Header options={options} />
       <CalendarController formattedDate={formattedDate} options={options} />
+      <div className="content-middle">
+        <Sidebar
+          users={users}
+          selfUser={users[0] ?? { id: 'self', number: '', name: '' }}
+        />
+        <div className="content-main" />
+      </div>
       {/* <h2>Accounts</h2>
 
       <ul className="account-list">
